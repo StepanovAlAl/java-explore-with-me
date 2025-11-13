85e605976795c1741e5f49dea9d2b70f7a1ca349
Vandal <arhvandal@yandex.ru>
2025-11-13 07:23:05 +0300

    main_svc check style updates

--- main-svc/src/main/java/ru/practicum/service/StatsService.java
+++ main-svc/src/main/java/ru/practicum/service/StatsService.java
@@ -1,7 +1,6 @@
 package ru.practicum.service;
 
 import jakarta.servlet.http.HttpServletRequest;
-import java.time.LocalDateTime;
 
 public interface StatsService {
     void saveHit(HttpServletRequest request);
